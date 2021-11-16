132b294d357e7ee807369b283445fa1708aea8b9
missathena <61482025+missathena@users.noreply.github.com>
2021-11-15 21:18:58 -0500

    timer and hover

--- Project 3/fifteen.js
+++ Project 3/fifteen.js
@@ -18,14 +18,31 @@ var ids_numeric = {
 
 var selected_background;
 
+function move() {
 
+}
+function hover(){
+     document.getElementById(ids[i]).className += " moveable"
+}
+var audio = new Audio('30 Second Timer With Jeopardy Thinking Music.mp3')
 function initializeGame() {
     selected_background = "super-mario"
-
-    
+    audio.play();
 
     for (var i = 0; i < ids.length - 1; i++) {
         document.getElementById(ids[i]).className = "tile " + "super-mario";
+
     }
 }
+
+var timeleft = 30;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    alert("Times Up!");
+  } else {
+    document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
 
