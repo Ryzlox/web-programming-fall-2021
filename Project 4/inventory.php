c616a969cbb07091a59691662b271ea86e1fc900
missathena <61482025+missathena@users.noreply.github.com>
2021-12-12 01:17:52 -0500

    athena

--- Project 4/inventory.php
+++ Project 4/inventory.php
@@ -6,7 +6,7 @@
     <title>Rent Car</title>
 </head>
 <body>
-<h1>Artist Database</h1>
+<h1>Pick a rental</h1>
 <?php
 $servername = "localhost";
 $username = "ajohns9";
@@ -19,14 +19,33 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
      die("Can't Connect: " . $conn->connect_error);
 }
+$sql = "SELECT carName, type, price_per_day from inventory";
+$result = $conn->query($sql);
+if($result->num_rows > 0){
 
-$
+    echo "<table class='cr'><tr><td><img src='BMW430i.jpg'></td><td>BMW 430i</td>";
+    echo "<td></td>";
+    echo "<tr></table>";
+
+    echo "<table class='e'><tr><th>Car</th><th>Type</th><th>Price Per Day</th></tr>";
+
+    while($row = $result->fetch_assoc()) {
+    		 $car=$row["carName"];
+    		 $type=$row["type"];
+    		 $price=$row["price_per_day"];
+
+             echo "<tr><td>".$car."</td><td>".$type."</td><td>".$price."</td></tr>";
+    }
+    	 echo "</table>";
+}else{
+    echo "0 results";
+}
+
+
 
 $conn->close();
 ?>
 
-<input type="button" onclick="location.href='add_artist.html'" value="Add Artist">
-<input type="button" onclick="location.href='add_album.html'" value="Add Album">
-<a href="login.php"><input type="button" value="Logout"></a>
+<input type="button" value="Confirm">
 </body>
 </html>
